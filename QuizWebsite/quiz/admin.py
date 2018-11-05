a91f6baca957697856e5225f47ea8e7df7c05208
dylanchhin <dylanchhinn@gmail.com>
2018-11-05 10:15:02 -0800

    Quiz Category, Questions on Admin site

--- QuizWebsite/quiz/admin.py
+++ QuizWebsite/quiz/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import AgeCategory
+from .models import Question
+
+admin.site.register(AgeCategory)
+admin.site.register(Question)
 # Register your models here.
--- QuizWebsite/quiz/models.py
+++ QuizWebsite/quiz/models.py
@@ -7,10 +7,14 @@ class AgeCategory(models.Model):
 	def __str__(self):
 		results = self.age + self.category
 		return results
+
 class Question(models.Model):
 	age_and_category =  models.ForeignKey(AgeCategory, on_delete=models.CASCADE)
 	QID = models.CharField(max_length=50)
+	problem = models.CharField(max_length=200, default = "")
 	answer = models.CharField(max_length=50)
 	choice1 = models.CharField(max_length=50)
 	choice2 = models.CharField(max_length=50)
 	choice3 = models.CharField(max_length=50)
+	def __str__(self):
+		return self.problem

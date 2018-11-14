7f6eb503e860209b1cbf2a78bb8cd0f0eadcaac0
dylanchhin <dylanchhinn@gmail.com>
2018-11-14 11:19:23 -0800

    Model

--- QuizWebsite/quiz/models.py
+++ QuizWebsite/quiz/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django_random_queryset import RandomManager
 # table for age and category
 class AgeCategory(models.Model):
 	age = models.CharField(max_length=50)
@@ -9,6 +9,7 @@ class AgeCategory(models.Model):
 		return results
 
 class Question(models.Model):
+	objects = RandomManager()
 	age_and_category =  models.ForeignKey(AgeCategory, on_delete=models.CASCADE)
 	QID = models.CharField(max_length=50)
 	problem = models.CharField(max_length=200, default = "")
@@ -18,3 +19,5 @@ class Question(models.Model):
 	choice3 = models.CharField(max_length=50)
 	def __str__(self):
 		return self.problem
+	#def choice1(self):
+		#return self.choice1
